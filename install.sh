06b183056e27ca315d96b8efc9b74d21703f09f9
katoshi <comamny@gmail.com>
2025-09-26 01:38:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ if [ "$(uname -m)" = "armv7l" ]; then
   echo "WARNING:"
   echo "The Moon Blockchain requires a 64 bit OS and this is 32 bit armv7l"
   echo "For more information, see"
-  echo "https://github.com/MOONCOINTEAM/moon-blockchain/wiki/Raspberry-Pi"
+  echo "https://github.com/comamny/moon-blockchain/wiki/Raspberry-Pi"
   echo "Exiting."
   exit 1
 fi
@@ -303,7 +303,7 @@ echo "For assistance join us on Keybase in the #support chat channel:"
 echo "https://keybase.io/team/moon_network.public"
 echo ""
 echo "Try the Quick Start Guide to running moon-blockchain:"
-echo "https://github.com/MOONCOINTEAM/moon-blockchain/wiki/Quick-Start-Guide"
+echo "https://github.com/comamny/moon-blockchain/wiki/Quick-Start-Guide"
 echo ""
 echo "To install the GUI type 'sh install-gui.sh' after '. ./activate'."
 echo ""
